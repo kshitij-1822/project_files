1f58d5c4ca5d9cd3581476ce97a32489803180ad
kshitij-1822 <mohitcharde22@gmail.com>
2026-01-28 20:12:23 +0530

    Add files via upload

---
+++ Javascript/Array/ex-1.js
@@ -0,0 +1,24 @@
+let array = [10,20,30]
+
+// update
+array[0]=50
+array[1]=60
+array[2]=70
+console.log(array)
+
+// insert using index
+
+array[3] = 80
+console.log(array)
+
+array[4] = 90
+console.log(array)
+
+
+// insert using function
+array.push(100)
+console.log(array)
+
+// add element on front
+array.unshift(110)
+console.log(array)
